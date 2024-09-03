a3ed828f36602cb7bd16841988c6e39e53556237
Eias <51035404+1Eias@users.noreply.github.com>
2024-09-03 10:00:49 +0200

    week 2 en 3

---
+++ bc_l2_opdracht2.py
@@ -0,0 +1,14 @@
+#dit lijn heeft dit gedaan: 15 + 4 = 19
+print(15+4)
+#dit lijn heeft dit gedaan: 15 - 4 = 11
+print(15-4)
+#dit lijn heeft dit gedaan: 15 x 4 = 60
+print(15*4)
+#dit lijn heeft dit gedaan: 15 : 4 = 3.75
+print(15/4)
+#3
+print(15//4)
+#50625
+print(15**4)
+#3
+print(15%4)
---
+++ btw berekenen.py
@@ -0,0 +1,12 @@
+Appels = 3.40
+Druiven = 2.45
+Bananen = 1.95
+
+Totaal = Appels + Druiven + Bananen
+BTW = Totaal/100*9
+
+print('Appels:', Appels)
+print('Druiven:', Druiven)
+print('Bananen:', Bananen)
+print('Totaal:', Totaal)
+print(BTW)

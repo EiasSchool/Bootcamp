71c07bfde23b96f8e514b3ef9203610a227fd3af
Eias <51035404+1Eias@users.noreply.github.com>
2024-09-03 10:52:30 +0200

    added folders to bootcamp

---
+++ Les 3 - Iets met gegevens/Opdracht 2/bc_l3_opdracht2.py
@@ -0,0 +1,6 @@
+getal1 = int(input("Voer getal 1 in: ")) #vraagt getal 1 op
+getal2 = int(input("Voer getal 2 in: ")) #vraagt getal 2 op
+getal3 = int(input("Voer getal 3 in: ")) #vraagt getal 3 op
+gemiddelde = (getal1 + getal2 + getal3) / 3 #tel alle waarden op en deel door 3
+
+print(gemiddelde) #print de gemiddelde

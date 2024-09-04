12b0a8441be72ef53f5b20e13fa793f8c1e20ed5
Eias <51035404+1Eias@users.noreply.github.com>
2024-09-04 09:12:10 +0200

    2.1 2.2 en 4.2

---
+++ Les 2 - Nu gaan we echt beginnen/Opdracht 2/bc_l2_opdracht2.py
@@ -0,0 +1,14 @@
+# Deze regel telt 15 en 4 op: 15 + 4 = 19
+print(15 + 4)
+# Deze regel trekt 4 af van 15: 15 - 4 = 11
+print(15 - 4)
+# Deze regel vermenigvuldigt 15 met 4: 15 * 4 = 60
+print(15 * 4)
+# Deze regel deelt 15 door 4, resulterend in een float: 15 / 4 = 3.75
+print(15 / 4)
+# Deze regel voert een gehele deling uit van 15 door 4: 15 // 4 = 3
+print(15 // 4)
+# Deze regel verheft 15 tot de macht 4: 15 ** 4 = 50625
+print(15 ** 4)
+# Deze regel vindt de rest van de deling van 15 door 4: 15 % 4 = 3
+print(15 % 4)
---
+++ Les 4 - Als dit, dan dat, anders…/Opdracht 2/bc_l4_opdracht2.py
@@ -0,0 +1,9 @@
+getal1 = int(input('Wat is de eerste getal? '))
+getal2 = int(input('Wat is de tweede getal? '))
+
+if getal1 > getal2:
+    print(f'{getal1} is grooter dan {getal2}')
+elif getal1 < getal2:
+    print(f'{getal1} is kleiner dan {getal2}')
+elif getal1 == getal2:
+    print(f'{getal1} is gelijk aan {getal2}')

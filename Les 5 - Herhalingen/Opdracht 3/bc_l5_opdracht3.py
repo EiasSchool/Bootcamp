b9ae09dd264cadfa7b699c0afa71d24074ffa15e
Eias <51035404+1Eias@users.noreply.github.com>
2024-09-05 20:16:40 +0200

    5.3 klaar

--- Les 5 - Herhalingen/Opdracht 3/bc_l5_opdracht3.py
+++ Les 5 - Herhalingen/Opdracht 3/bc_l5_opdracht3.py
@@ -1,11 +1,24 @@
 from random import randint
 
-te_raden = randint(1,5)
+totale_score = 0
 
-while True:
-    gok = int(input('Kies een getal van 1 t/m 5: '))
-    if gok == te_raden:
-        print("Je hebt het getal goed geraden!")
-        break
-    else:
-        print("Je hebt het getal niet goed geraden!")
+for ronde in range(3):
+
+    print(f'Ronde {ronde + 1}')
+    te_raden = randint(1,5)
+
+    aantal_gokken = 0
+
+    while True:
+        gok = int(input('Kies een getal van 1 t/m 5: '))
+        if gok == te_raden:
+            print("Je hebt het getal goed geraden!")
+            break
+        else:
+            print("Je hebt het getal niet goed geraden!")
+            aantal_gokken += 1
+
+    score = 20 - aantal_gokken
+    totale_score += score
+
+print(f'Jouw Score: {totale_score}')

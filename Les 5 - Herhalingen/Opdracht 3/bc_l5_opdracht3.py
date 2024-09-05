8fd209724d6ff67e5e9e0a982d27459c40231385
Eias <51035404+1Eias@users.noreply.github.com>
2024-09-05 19:39:41 +0200

    5.3 loop gedaan

--- Les 5 - Herhalingen/Opdracht 3/bc_l5_opdracht3.py
+++ Les 5 - Herhalingen/Opdracht 3/bc_l5_opdracht3.py
@@ -1,9 +1,11 @@
 from random import randint
 
 te_raden = randint(1,5)
-gok = int(input('Kies een getal van 1 t/m 5: '))
 
-if (te_raden == gok):
-    print("Je hebt het getal goed geraden!")
-else:
-    print("Je hebt het getal niet goed geraden!")
+while True:
+    gok = int(input('Kies een getal van 1 t/m 5: '))
+    if gok == te_raden:
+        print("Je hebt het getal goed geraden!")
+        break
+    else:
+        print("Je hebt het getal niet goed geraden!")

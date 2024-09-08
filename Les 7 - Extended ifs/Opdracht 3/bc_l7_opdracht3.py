4f729d91fa792373db2819e206cd7e06ad8b5b74
Eias <51035404+1Eias@users.noreply.github.com>
2024-09-08 17:26:18 +0200

    7.3 klaar

---
+++ Les 7 - Extended ifs/Opdracht 3/bc_l7_opdracht3.py
@@ -0,0 +1,6 @@
+leeftijd = int(input('Wat is je leeftijd?\n'))
+
+if not (leeftijd > 18):
+    print("Sorry, je hebt geen toegang tot het evenement.")
+else:
+    print("Welkom! Je hebt toegang tot het evenement.")
